08aec1357390f6027e36135d1101c2e149c4d8ae
Uk-jake <mag0225@naver.com>
2025-06-04 09:55:25 +0300

    add: PG_L2_기능개발

--- src/2025-06/PG_L2_기능개발_20250604.java
+++ src/2025-06/PG_L2_기능개발_20250604.java
@@ -1,10 +1,6 @@
-package test;
-
-import java.io.*;
-import java.security.Key;
 import java.util.*;
 
-public class RunSolution {
+public class PG_L2_기능개발_20250604 {
     public static void main(String[] args) {
 
         int[] progresses = {93, 30, 55};
